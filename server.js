7276b16d67eeec85721c82a4d64e578852d664a8
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-09-01 00:39:45 +0530

    Add basic Node.js server

--- server.js
+++ server.js
@@ -3,10 +3,32 @@ const http = require('http');
 
 //Create an HTTP server and define how it handles requests
  const server = http.createServer((req,res) => {
-   console.log(req);
+   console.log(req.url, req.method, req.headers);
+   if(req.url === '/'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete coding</title></head>');
+    res.write('<body><h1>Welcome to home</h1></body>');
+    res.write('</html>')
+    return res.end();
+   }else if(req.url === '/products'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete coding</title></head>');
+    res.write('<body><h1>check out product</h1></body>');
+    res.write('</html>')
+    return res.end();
+   }
+   res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete coding</title></head>');
+    res.write('<body><h1>like / share / subscribe</h1></body>');
+    res.write('</html>')
+    return res.end();
+   //process.exist(); // stop event loop
   });
    // Define the port number where the server will run
-  const PORT = 3001;
+  const PORT = 3000;
 
   //Start the server and listen on the specified port
   server.listen(PORT,() => {
