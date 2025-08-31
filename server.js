35ff1691a04bd06c10776f1c11cc63990be3506a
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-08-31 23:34:56 +0530

    Add basic Node.js server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+//Import the built-in HTTP module
 const http = require('http');
 
-// function requestListener(request,response){
-//   console.log(request)
-// } old style  
-  
+//Create an HTTP server and define how it handles requests
  const server = http.createServer((req,res) => {
    console.log(req);
   });
+   // Define the port number where the server will run
   const PORT = 3001;
+
+  //Start the server and listen on the specified port
   server.listen(PORT,() => {
     console.log(`server running on address http://localhost:${PORT}`)
   });
