4478a00a0f7aa2ebbc21b9f02d27ad022a1a2c60
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-09-07 22:40:07 +0530

    Blocking vs Async

---
+++ pra2.calculator/pra3.Async.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+console.log('1. Start of script');
+
+// synchronous (blocking) operation
+console.log('2. Reading file synchronously');
+const dataSync = fs.readFileSync('user-details.txt', 'utf8');
+console.log('3. Synchronous read complete');
+
+// asynchronous (non-blocking) operation
+console.log('4. Reading file asynchronously');
+fs.readFile('user-details.txt', 'utf8', (err, dataAsync) => {
+  if (err) throw err;
+  console.log('6. Asynchronous read complete');
+});
+console.log('5. End of script');
+
