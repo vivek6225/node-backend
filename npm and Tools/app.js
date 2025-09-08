1d8a4d7a7eaf003598f9d3f4d38cb5c3f0c898e7
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-09-09 00:20:29 +0530

    npm and Tools

---
+++ npm and Tools/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end();
+
+});
+const PORT = 3000;
+server.listen(PORT, () =>{
+  console.log(`server running on address http://localhost:${PORT}`);
+});
