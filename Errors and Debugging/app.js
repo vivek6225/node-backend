ded323fd20233451ea0d9f6fe5b1aff388beab7f
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-09-09 20:25:01 +0530

    feat: setup basic HTTP server with syntax module

--- Errors and Debugging/app.js
+++ Errors and Debugging/app.js
@@ -1,10 +1,25 @@
+//// Import the built-in http module
 const http = require('http');
+
+// Import your custom module (syntax.js file)
 const testingSyntax = require('./syntax');
+
+// Create an HTTP server
 const server = http.createServer((req, res) =>{
-  comsole.log(req.url, req.method);
+
+    // Log request details (URL and method like GET, POST, etc.)
+  console.log(req.url, req.method);
+
+   // Call the function from syntax.js
   testingSyntax();
 });
+
+// Define the port number
 const PORT = 3000;
+
+// Start the server and listen on the given port
 server.listen(PORT, () =>{
-console.log(`Server running on address http://localhost:{PORT}`)
+
+   // Log confirmation message when server starts
+console.log(`Server running on address http://localhost:${PORT}`)
 });
